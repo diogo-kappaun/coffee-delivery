ca9c548a6c00024fd0742686dad5c9b0258eef3a
diogo-kappaun <diogohkappaun@gmail.com>
2024-10-20 09:38:19 -0300

    :sparkles: feat: Create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import Logo from '@/assets/logo.svg'
+import { Location } from '@/components/Location'
+import { Link } from 'react-router-dom'
+import { ButtonCart } from './ButtonCart'
+
+export const Header = () => {
+  return (
+    <div className="flex justify-between p-8">
+      <Link to="/">
+        <img src={Logo} alt="Logo da Coffee Delivery" />
+      </Link>
+      <div className="flex gap-3">
+        <Location city="Maravilha" state="SC" />
+        <ButtonCart amountCart={3} />
+      </div>
+    </div>
+  )
+}
