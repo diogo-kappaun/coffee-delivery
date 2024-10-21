39c657b866b4e16721cda28d2e21b7435c5b7721
diogo-kappaun <diogohkappaun@gmail.com>
2024-10-21 10:06:32 -0300

    :recycle: refactor: Add Tailwind variants to create variations

---
+++ src/components/ButtonCart.tsx
@@ -0,0 +1,40 @@
+import { Button } from '@/components/ui/button'
+import { PiShoppingCartFill } from 'react-icons/pi'
+import { Link } from 'react-router-dom'
+import { tv } from 'tailwind-variants'
+
+interface ButtonCartProps {
+  amountCart: number
+  withAmount?: boolean
+  variant?: 'header' | 'card'
+}
+
+export const ButtonCart = ({
+  amountCart,
+  withAmount = false,
+  variant = 'card',
+}: ButtonCartProps) => {
+  const buttonCart = tv({
+    base: 'h-10 w-10 p-2',
+    variants: {
+      variant: {
+        header:
+          'bg-product-yellow-light text-product-yellow-dark group-hover:bg-product-yellow-light/80 group-hover:text-product-yellow-dark/80',
+        card: 'bg-product-purple-dark text-background group-hover:bg-product-purple-dark/80 group-hover:text-background/80',
+      },
+    },
+  })
+
+  return (
+    <Link className="group relative" to="/cart">
+      {withAmount && (
+        <div className="absolute -right-2 -top-2 flex h-5 w-5 items-center justify-center rounded-full bg-product-yellow-dark text-xs font-bold text-white group-hover:bg-product-yellow-dark/80">
+          {amountCart}
+        </div>
+      )}
+      <Button className={buttonCart({ variant })}>
+        <PiShoppingCartFill size={22} />
+      </Button>
+    </Link>
+  )
+}
