e21ab876b743a6de7aa71e96008668894a431aeb
diogo-kappaun <diogohkappaun@gmail.com>
2024-10-22 15:20:58 -0300

    :sparkles: feat: Add new routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,7 @@
 import { AppLayout } from '@/layout/AppLayout'
+import { Checkout } from '@/pages/Checkout'
 import { Home } from '@/pages/Home'
+import { Success } from '@/pages/Success'
 
 import { Route, Routes } from 'react-router-dom'
 
@@ -8,6 +10,8 @@ const AppRouter = () => {
     <Routes>
       <Route path="/" element={<AppLayout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/success" element={<Success />} />
       </Route>
     </Routes>
   )
