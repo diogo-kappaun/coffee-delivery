39eb2789cb2b8402e281269112530cf32964f0a6
diogo-kappaun <diogohkappaun@gmail.com>
2024-10-22 13:58:49 -0300

    :sparkles: feat: Create Delivery form

---
+++ src/pages/Checkout/components/DeliveryForm.tsx
@@ -0,0 +1,112 @@
+import { Form, FormControl, FormField, FormItem } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { DeliveryAddressSchema } from '@/schemas/indes'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+
+export const DeliveryForm = () => {
+  const form = useForm<z.infer<typeof DeliveryAddressSchema>>({
+    resolver: zodResolver(DeliveryAddressSchema),
+    defaultValues: {
+      city: '',
+      complement: '',
+      district: '',
+      number: '',
+      state: '',
+      street: '',
+      zipCode: '',
+    },
+  })
+
+  const onSubmit = (values: z.infer<typeof DeliveryAddressSchema>) => {
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="min-[500px]:grid-cols-form mt-8 flex flex-col gap-[16px_12px] min-[500px]:grid"
+      >
+        <FormField
+          control={form.control}
+          name="zipCode"
+          render={(field) => (
+            <FormItem>
+              <FormControl>
+                <Input {...field} placeholder="CEP" type="text" />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="street"
+          render={(field) => (
+            <FormItem className="col-span-3 row-start-2 row-end-2">
+              <FormControl>
+                <Input {...field} placeholder="Rua" type="text" />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="number"
+          render={(field) => (
+            <FormItem className="row-start-3 row-end-3">
+              <FormControl>
+                <Input {...field} placeholder="Número" type="text" />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="complement"
+          render={(field) => (
+            <FormItem className="col-span-2 row-start-3 row-end-3">
+              <FormControl>
+                <Input {...field} placeholder="Complemento" type="text" />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="district"
+          render={(field) => (
+            <FormItem className="row-start-4 row-end-4">
+              <FormControl>
+                <Input {...field} placeholder="Bairro" type="text" />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="city"
+          render={(field) => (
+            <FormItem className="row-start-4 row-end-4">
+              <FormControl>
+                <Input {...field} placeholder="Cidade" type="text" />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="state"
+          render={(field) => (
+            <FormItem className="row-start-4 row-end-4">
+              <FormControl>
+                <Input {...field} placeholder="UF" type="text" />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  )
+}
---
+++ src/schemas/indes.ts
@@ -0,0 +1,13 @@
+import * as z from 'zod'
+
+export const DeliveryAddressSchema = z.object({
+  zipCode: z
+    .string()
+    .min(9, { message: 'Por favor informe um CEP válido (00000-000)' }),
+  street: z.string().min(1, { message: 'Informe uma rua válida!' }),
+  number: z.string().min(1, { message: 'Informe um número válido!' }),
+  complement: z.string().optional(),
+  district: z.string().min(1, { message: 'Informe um bairro válido!' }),
+  city: z.string().min(1, { message: 'Informe uma cidade válida!' }),
+  state: z.string().min(1, { message: 'Informe um estado válido!' }),
+})
