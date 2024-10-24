84e1aceea09b0aa7c4baea029bb9246c86d18879
diogo-kappaun <diogohkappaun@gmail.com>
2024-10-24 08:39:01 -0300

    :sparkles: feat(ui): disable checkout link when cart is empty

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,12 @@
 import Logo from '@/assets/logo.svg'
+import { useCart } from '@/cart'
 import { ButtonCart } from '@/components/ButtonCart'
 import { Location } from '@/components/Header/Location'
 import { Link } from 'react-router-dom'
 
 export const Header = () => {
+  const { items } = useCart()
+
   return (
     <div className="mx-auto flex w-page items-center justify-between gap-2 px-5 py-8">
       <Link to="/">
@@ -11,8 +14,11 @@ export const Header = () => {
       </Link>
       <div className="flex gap-3">
         <Location city="Maravilha" state="SC" />
-        <Link to="/checkout">
-          <ButtonCart variant="header" withAmount amountCart={3} />
+        <Link
+          className={items.length === 0 ? 'pointer-events-none opacity-80' : ''}
+          to="/checkout"
+        >
+          <ButtonCart variant="header" withAmount amountCart={items.length} />
         </Link>
       </div>
     </div>
