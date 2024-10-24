161862e6fa2ca4c73418e0eaa23f38aced56aced
diogo-kappaun <diogohkappaun@gmail.com>
2024-10-24 10:14:24 -0300

    :recycle: refactor: Calculate total quantity of items

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,6 +7,11 @@ import { Link } from 'react-router-dom'
 export const Header = () => {
   const { items } = useCart()
 
+  const itemsQuantity = items.reduce(
+    (accumulator, item) => accumulator + item.quantity,
+    0,
+  )
+
   return (
     <div className="mx-auto flex w-page items-center justify-between gap-2 px-5 py-8">
       <Link to="/">
@@ -18,7 +23,7 @@ export const Header = () => {
           className={items.length === 0 ? 'pointer-events-none opacity-80' : ''}
           to="/checkout"
         >
-          <ButtonCart variant="header" withAmount amountCart={items.length} />
+          <ButtonCart variant="header" withAmount amountCart={itemsQuantity} />
         </Link>
       </div>
     </div>
