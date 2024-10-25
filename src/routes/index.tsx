5216b7faa774b8d0623783bbaca4fadf9b94aff2
diogo-kappaun <diogohkappaun@gmail.com>
2024-10-25 14:41:14 -0300

    :sparkles: feat(success): Limit route when no orders

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,16 +2,18 @@ import { AppLayout } from '@/layout/AppLayout'
 import { Checkout } from '@/pages/Checkout'
 import { Home } from '@/pages/Home'
 import { Success } from '@/pages/Success'
+import { useOrder } from '@/store/order'
 
 import { Route, Routes } from 'react-router-dom'
 
 const AppRouter = () => {
+  const { order } = useOrder()
   return (
     <Routes>
       <Route path="/" element={<AppLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/checkout" element={<Checkout />} />
-        <Route path="/success" element={<Success />} />
+        {order && <Route path="/success" element={<Success />} />}
       </Route>
     </Routes>
   )
