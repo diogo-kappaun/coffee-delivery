c5d52575e9db59ead9dc278da6f383e22ef868c6
diogo-kappaun <diogohkappaun@gmail.com>
2024-10-25 14:41:39 -0300

    :sparkles: feat(success): Add Success logic

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,7 +1,17 @@
 import motocycle from '@/assets/motocycle.svg'
+import { useOrder } from '@/store/order'
 import { PiMapPinFill } from 'react-icons/pi'
 
 export const Success = () => {
+  const { order } = useOrder()
+  const { street, number, city, district, state, method } = order!.delivery
+
+  const paymentMethod = {
+    credit: 'Cartão de Crédito',
+    debit: 'Cartão de Débito',
+    money: 'Dinheiro',
+  }
+
   return (
     <main className="mx-auto mt-20 w-page space-y-10 px-5 pb-12">
       <div className="space-y-1">
@@ -22,9 +32,14 @@ export const Success = () => {
               </div>
               <div className="flex flex-col">
                 <span>
-                  Entrega em <strong>Rua João Daniel Martinelli, 102</strong>
+                  Entrega em{' '}
+                  <strong>
+                    Rua {street}, {number}
+                  </strong>
                 </span>
-                <span>Farrapos - Porto Alegre, RS</span>
+                <span>
+                  {district} - {city}, {state}
+                </span>
               </div>
             </div>
             <div className="flex items-center gap-3">
@@ -42,7 +57,7 @@ export const Success = () => {
               </div>
               <div className="flex flex-col">
                 <span>Pagamento na entrega</span>
-                <strong>Cartão de Crédito</strong>
+                <strong>{paymentMethod[method]}</strong>
               </div>
             </div>
           </div>
